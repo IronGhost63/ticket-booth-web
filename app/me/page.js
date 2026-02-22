f7007cd6f8921d9e335e5414352305432cc8ca1f
Jay <me@jirayu.in.th>
2026-02-22 16:17:13 +0700

    base profile page

--- app/me/page.js
+++ app/me/page.js
@@ -1,11 +1,14 @@
 "use client"
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { redirect, RedirectType } from "next/navigation";
 import { isLoggedIn } from "@/app/lib/api";
+import Link from "next/link";
 import Layout from "@/app/ui/layout/main";
 
 export default function Page() {
+  const [view, setView] = useState('profile');
+
   useEffect(() => {
     if ( !isLoggedIn() ) {
       redirect("/signin", RedirectType.replace);
@@ -17,7 +20,29 @@ export default function Page() {
       <main>
         <div className="main-container">
           <h1 className="text-5xl font-medium text-center mb-6">My Account</h1>
-
+          <ul className="user-menu">
+            <li className="user-menu-item">
+              <button className="user-menu-button button outlined dark" onClick={() => setView('profile')}>Profile</button>
+            </li>
+            <li className="user-menu-item">
+              <button className="user-menu-button button outlined dark" onClick={() => setView('tickets')}>Tickets</button>
+            </li>
+            <li className="user-menu-item">
+              <Link href="/signout" className="user-menu-button button outlined dark">Sign out</Link>
+            </li>
+          </ul>
+          <div className="content">
+            {view === 'profile' && (
+              <div>
+                Profile view
+              </div>
+            )}
+            {view === 'tickets' && (
+              <div>
+                Tickets view
+              </div>
+            )}
+          </div>
         </div>
       </main>
     </Layout>
